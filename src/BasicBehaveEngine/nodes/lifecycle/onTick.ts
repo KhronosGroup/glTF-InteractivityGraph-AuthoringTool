a4b0564b1419e0e7d2bb6f9337e1ff7576054773
Robert Dorn <dorn@prefrontalcortex.de>
2024-11-27 15:04:59 +0100

    fixed NaN on first process in onTick timeSinceLastTick

--- src/BasicBehaveEngine/nodes/lifecycle/onTick.ts
+++ src/BasicBehaveEngine/nodes/lifecycle/onTick.ts
@@ -17,7 +17,7 @@ export class OnTickNode extends BehaveEngineNode {
         const tickTime = Date.now();
         if (isNaN(this._startTime)) {
             this.outValues.timeSinceStart = {id: "timeSinceStart", value: [0], type: this.getTypeIndex('float')};
-            this.outValues.timeSinceLastTick = {id: "timeSinceLastTick", value: [NaN], type: this.getTypeIndex('float')};
+            this.outValues.timeSinceLastTick = {id: "timeSinceLastTick", value: [0], type: this.getTypeIndex('float')};
             this._startTime = tickTime;
         } else {
             this.outValues.timeSinceStart = {id: "timeSinceStart", value: [tickTime - this._startTime], type: this.getTypeIndex('float')};

ab9c23c2e01b3211d034eee2360f0d2f617383b4
mattmacf98 <macfarqu@amazon.com>
2025-03-14 11:51:00 -0400

    improve formatting of experimental debug node

--- src/BasicBehaveEngine/nodes/experimental/Debug.ts
+++ src/BasicBehaveEngine/nodes/experimental/Debug.ts
@@ -1,5 +1,6 @@
 
 import { IInteractivityValue } from "../../../types/InteractivityGraph";
+import { standardTypes } from "../../../types/nodes";
 import {BehaveEngineNode, IBehaviourNodeProps} from "../../BehaveEngineNode";
 
 export class DebugLog extends BehaveEngineNode {
@@ -43,7 +44,10 @@ export class DebugLog extends BehaveEngineNode {
     populateTemplate(template: string, vals: any): string {
         let templateCopy = template
         for (const val of Object.keys(vals)) {
-            templateCopy = templateCopy.replace(`{${val}}`, vals[val]);
+            console.log(this.values[val]);
+            const typeName = this.getType(this.values[val].type!);
+            console.log(typeName);
+            templateCopy = templateCopy.replace(`{${val}}`, formatValue(vals[val], typeName));
         }
         return templateCopy;
     }
@@ -59,5 +63,40 @@ export class DebugLog extends BehaveEngineNode {
         console.log(populatedTemplate);
 
         super.processNode(flowSocket);
+    }
+}
+
+function formatValue(value: any, typeName: string): string {
+    switch (typeName) {
+        case "bool":
+        case "int":
+        case "float":
+            return value.toString();
+        case "float2":
+            return `[${value[0]}, ${value[1]}]`;
+        case "float3":
+            return `[${value[0]}, ${value[1]}, ${value[2]}]`;
+        case "float4":
+            return `[${value[0]}, ${value[1]}, ${value[2]}, ${value[3]}]`;
+        case "float2x2":
+            return `[
+                [${value[0][0]}, ${value[1][0]}],
+                [${value[0][1]}, ${value[1][1]}]
+            ]`;
+        case "float3x3":
+            return `[
+                [${value[0][0]}, ${value[1][0]}, ${value[2][0]}],
+                [${value[0][1]}, ${value[1][1]}, ${value[2][1]}],
+                [${value[0][2]}, ${value[1][2]}, ${value[2][2]}]
+            ]`;
+        case "float4x4":
+            return `[
+                [${value[0][0]}, ${value[1][0]}, ${value[2][0]}, ${value[3][0]}],
+                [${value[0][1]}, ${value[1][1]}, ${value[2][1]}, ${value[3][1]}],
+                [${value[0][2]}, ${value[1][2]}, ${value[2][2]}, ${value[3][2]}],
+                [${value[0][3]}, ${value[1][3]}, ${value[2][3]}, ${value[3][3]}]
+            ]`;
+        default:
+            return value.toString();
     }
 }

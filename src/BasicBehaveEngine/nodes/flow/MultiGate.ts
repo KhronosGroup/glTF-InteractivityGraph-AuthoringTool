5da6e60e89ac6420c2f200ae868edebd67a99b2c
Patrick Härtl <haertl@ux3d.io>
2025-10-22 15:06:14 +0200

    Fix handling of loop and random in multiGate

--- src/BasicBehaveEngine/nodes/flow/MultiGate.ts
+++ src/BasicBehaveEngine/nodes/flow/MultiGate.ts
@@ -1,11 +1,11 @@
 import {BehaveEngineNode, IBehaviourNodeProps} from "../../BehaveEngineNode";
 
 export class MultiGate extends BehaveEngineNode {
-    REQUIRED_CONFIGURATIONS = {isRandom: {defaultValue: [false]}, loop: {defaultValue: [false]}}
+    REQUIRED_CONFIGURATIONS = {isRandom: {defaultValue: [false]}, isLoop: {defaultValue: [false]}}
     _numberOutputFlows: number;
     _currentIndex: number;
     _isRandom: boolean;
-    _loop: boolean;
+    _isLoop: boolean;
     _orderedOutFlows: string[];
     _unSeenOutIndexes: number[];
 
@@ -15,13 +15,13 @@ export class MultiGate extends BehaveEngineNode {
         this.validateValues(this.values);
         this.validateConfigurations(this.configuration);
 
-        const { isRandom, loop} = this.evaluateAllConfigurations(Object.keys(this.REQUIRED_CONFIGURATIONS));
+        const { isRandom, isLoop} = this.evaluateAllConfigurations(Object.keys(this.REQUIRED_CONFIGURATIONS));
         this._numberOutputFlows = Object.keys(this.flows).length;
         this._isRandom = isRandom[0];
-        this._loop = loop[0];
+        this._isLoop = isLoop[0];
         this._orderedOutFlows =  Object.keys(this.flows).sort();
         this._unSeenOutIndexes = Array(this._numberOutputFlows).fill(0).map((_, index) => index);
-        this._currentIndex = 0;
+        this._currentIndex = this._isRandom ? Math.floor(Math.random() * this._unSeenOutIndexes.length) : 0;
 
         this.outValues.lastIndex = { value: [-1], type: this.getTypeIndex('int')};
     }
@@ -31,7 +31,7 @@ export class MultiGate extends BehaveEngineNode {
         this.graphEngine.processNodeStarted(this);
 
         if (flowSocket === "reset") {
-            this._currentIndex = 0;
+            this._currentIndex = this._isRandom ? Math.floor(Math.random() * this._unSeenOutIndexes.length) : 0;
             this._unSeenOutIndexes = Array(this._numberOutputFlows).fill(0).map((_, index) => index);
             return;
         }
@@ -45,17 +45,18 @@ export class MultiGate extends BehaveEngineNode {
         this.outValues.lastIndex = { value: [this._unSeenOutIndexes[this._currentIndex]], type: this.getTypeIndex('int')};
         this.processFlow(this.flows[currentFlowName]);
 
+        this._unSeenOutIndexes.splice(this._currentIndex, 1);
+
         if (this._unSeenOutIndexes.length === 0) {
-            if (this._loop) {
+            if (this._isLoop) {
                 this._unSeenOutIndexes = Array(this._numberOutputFlows).fill(0).map((_, index) => index);
             } else {
                 return;
             }
         }
 
-        this._unSeenOutIndexes.splice(this._currentIndex, 1);
         if (this._isRandom) {
-            this._currentIndex = this._unSeenOutIndexes[Math.floor(Math.random() * this._unSeenOutIndexes.length)];
+            this._currentIndex = Math.floor(Math.random() * this._unSeenOutIndexes.length);
         } else {
             this._currentIndex = (this._currentIndex) % this._unSeenOutIndexes.length;
         }
